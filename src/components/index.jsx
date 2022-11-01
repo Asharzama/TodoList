5c76c09124eb7ea86504a02fb00c712d53d34339
Ashar zama Khan <asharzamakhan01@gmail.com>
2022-11-02 00:31:10 +0530

    changes made in css

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../App.css";
 import { AiFillEdit, AiOutlinePlus } from "react-icons/ai";
-import { MdDelete,MdCheck } from "react-icons/md";
+import { MdDelete, MdCheck } from "react-icons/md";
 import { useRef } from "react";
 import { useEffect } from "react";
 
@@ -13,19 +13,17 @@ export function Todo(props) {
   const [sortItems, setSortItems] = useState("All");
   const [displayList, setDisplayList] = useState([]);
 
-  useEffect(()=>{
-    if(sortItems === "Completed"){
+  useEffect(() => {
+    if (sortItems === "Completed") {
       let copy = [...todoList];
-      setDisplayList(copy.filter((item)=>item.isChecked));
-    }
-    else if(sortItems === "Incomplete"){
+      setDisplayList(copy.filter((item) => item.isChecked));
+    } else if (sortItems === "Incomplete") {
       let copy = [...todoList];
-      setDisplayList(copy.filter((item)=> !item.isChecked));
-    }
-    else{
+      setDisplayList(copy.filter((item) => !item.isChecked));
+    } else {
       setDisplayList(todoList);
     }
-  },[sortItems, todoList])
+  }, [sortItems, todoList]);
 
   const inputRef = useRef(null);
 
@@ -85,7 +83,8 @@ export function Todo(props) {
     <div id="container">
       <h1>TO DO List</h1>
       <div className="input-wrapper">
-        <input ref={inputRef}
+        <input
+          ref={inputRef}
           type={"text"}
           className="text"
           placeholder={props.placeholder}
@@ -101,28 +100,39 @@ export function Todo(props) {
         <button className="add" onClick={addTodo}>
           <AiOutlinePlus size="2.5em" />
         </button>
-        <select className="options" onChange={(e)=>setSortItems(e.target.value)}>
+        <select
+          className="options"
+          onChange={(e) => setSortItems(e.target.value)}
+        >
           <option value="All">All</option>
           <option value="Completed">Completed</option>
           <option value="Incomplete">Incomplete</option>
         </select>
       </div>
 
-      <p style={{ visibility: `${alert}`, color: "red" }}>Please Add list Item</p>
+      <p style={{ visibility: `${alert}`, color: "red" }}>
+        Please Add list Item
+      </p>
 
       {displayList.map((todo, index) => (
         <div id="item" key={index}>
-          <span className="item" >{todo.work}</span>
+          <span className="item">{todo.work}</span>
           <div>
             <button className="Checked" onClick={() => checkBox(todo, index)}>
-              <MdCheck size="1.5em"/><span className="tooltiptext">Task Completed</span>
+              <MdCheck size="1.5em" />
+              <span className="tooltiptext">Task Completed</span>
             </button>
-            <button className="Edit" onClick={() => editItemHandler(todo, index)}>
+            <button
+              className="Edit"
+              onClick={() => editItemHandler(todo, index)}
+            >
               <AiFillEdit size="1.5em" />
-            <span className="tooltiptext">Edit Task</span></button>
+              <span className="tooltiptext">Edit Task</span>
+            </button>
 
             <button className="Delete" onClick={() => deleteItem(index)}>
-              <MdDelete size="1.5em" /><span className="tooltiptext">Delete Task</span>
+              <MdDelete size="1.5em" />
+              <span className="tooltiptext">Delete Task</span>
             </button>
           </div>
         </div>

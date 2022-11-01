96f78c3f3879dfe33d2db9e98223686cfb38e74f
Ashar zama Khan <asharzamakhan01@gmail.com>
2022-11-01 19:21:02 +0530

    added select

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import "../App.css";
 import { AiFillEdit, AiOutlinePlus } from "react-icons/ai";
-import { MdDelete } from "react-icons/md";
+import { MdDelete,MdCheck } from "react-icons/md";
 import { useRef } from "react";
+import { useEffect } from "react";
 
 export function Todo(props) {
   const [todoList, setTodoList] = useState([]);
   const [todo, setTodo] = useState("");
   const [editItem, setEditItem] = useState("");
   const [alert, setAlert] = useState("hidden");
+  const [sortItems, setSortItems] = useState("All");
+  const [displayList, setDisplayList] = useState([]);
+
+  useEffect(()=>{
+    if(sortItems === "Completed"){
+      let copy = [...todoList];
+      setDisplayList(copy.filter((item)=>item.isChecked));
+    }
+    else if(sortItems === "Incomplete"){
+      let copy = [...todoList];
+      setDisplayList(copy.filter((item)=> !item.isChecked));
+    }
+    else{
+      setDisplayList(todoList);
+    }
+  },[sortItems, todoList])
 
   const inputRef = useRef(null);
 
@@ -84,21 +101,28 @@ export function Todo(props) {
         <button className="add" onClick={addTodo}>
           <AiOutlinePlus size="2.5em" />
         </button>
+        <select className="options" onChange={(e)=>setSortItems(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Completed">Completed</option>
+          <option value="Incomplete">Incomplete</option>
+        </select>
       </div>
 
       <p style={{ visibility: `${alert}`, color: "red" }}>Please Add list Item</p>
 
-      {todoList.map((todo, index) => (
+      {displayList.map((todo, index) => (
         <div id="item" key={index}>
-          <span className="item" onClick={() => checkBox(todo, index)}>{todo.work}</span>
+          <span className="item" >{todo.work}</span>
           <div>
-
-            <button onClick={() => editItemHandler(todo, index)}>
-              <AiFillEdit size="1.5em" />
+            <button className="Checked" onClick={() => checkBox(todo, index)}>
+              <MdCheck size="1.5em"/><span className="tooltiptext">Task Completed</span>
             </button>
+            <button className="Edit" onClick={() => editItemHandler(todo, index)}>
+              <AiFillEdit size="1.5em" />
+            <span className="tooltiptext">Edit Task</span></button>
 
-            <button onClick={() => deleteItem(index)}>
-              <MdDelete size="1.5em" />
+            <button className="Delete" onClick={() => deleteItem(index)}>
+              <MdDelete size="1.5em" /><span className="tooltiptext">Delete Task</span>
             </button>
           </div>
         </div>
